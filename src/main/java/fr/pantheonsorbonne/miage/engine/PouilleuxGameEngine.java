53db31548bd631b497b1069a0933775cc82f0a11
Câm-Vi Nguyen <167216844+camviweb@users.noreply.github.com>
2024-12-15 14:39:35 +0100

    add paire de 10

--- src/main/java/fr/pantheonsorbonne/miage/engine/PouilleuxGameEngine.java
+++ src/main/java/fr/pantheonsorbonne/miage/engine/PouilleuxGameEngine.java
@@ -17,35 +17,33 @@ public abstract class PouilleuxGameEngine {
 
     public void play() {
         int numberOfPlayers = getInitialPlayers().size();
-        int handSize = 51/numberOfPlayers; //comment trouver deck.size? pour ne pas avoir le variable
+        int handSize = 51 / numberOfPlayers; // comment trouver deck.size? pour ne pas avoir le variable
         int remainingDeck = 51;
         for (String playerName : getInitialPlayers()) {
             Card[] cards;
-            if((remainingDeck)%numberOfPlayers == 0){
+            if ((remainingDeck) % numberOfPlayers == 0) {
                 cards = deck.getCards(handSize);
                 remainingDeck -= handSize;
-            }
-            else{
-                cards = deck.getCards(handSize+1);
+            } else {
+                cards = deck.getCards(handSize + 1);
                 remainingDeck -= (handSize + 1);
             }
             numberOfPlayers--;
-        String hand = Card.cardsToString(cards);
-        giveCardsToPlayer(playerName, hand);
+            String hand = Card.cardsToString(cards);
+            giveCardsToPlayer(playerName, hand);
         }
 
         final Deque<String> players = new LinkedList<>();
         players.addAll(this.getInitialPlayers());
-        
 
-        for(String player : players){
-             while(findPairs(player) != null){
+        for (String player : players) {
+            while (findPairs(player) != null) {
                 removePairsFromPlayer(player);
-             }
+            }
         }
 
         String winner = "";
-        //int initialPlayerSize = players.size();
+        // int initialPlayerSize = players.size();
 
         while (true) {
             String firstPlayerInRound = players.poll();
@@ -54,35 +52,36 @@ public abstract class PouilleuxGameEngine {
             String secondPlayerInRound = players.poll();
             players.addFirst(secondPlayerInRound);
             String play;
-            if ((play = playRound(players, firstPlayerInRound, secondPlayerInRound)) != ""){
+            if ((play = playRound(players, firstPlayerInRound, secondPlayerInRound)) != "") {
                 winner = play;
                 declareWinner(winner);
-                for (String player : players){
-                    if(player != winner && checkLoser(player)){
+                for (String player : players) {
+                    if (player != winner && checkLoser(player)) {
                         declareLoser(player);
                     }
                 }
                 break;
             }
-            
+
         }
-        //since we've left the loop, we have only 1 player left: the winner
-        //send him the gameover and leave
-        
+        // since we've left the loop, we have only 1 player left: the winner
+        // send him the gameover and leave
+
     }
 
     protected abstract List<String> getInitialPlayers();
 
     protected abstract void giveCardsToPlayer(String playerName, String hand);
 
-    protected String playRound(Queue<String> players, String firstPlayerInRound, String secondPlayerInRound) {
+    protected String playRound(Deque<String> players, String firstPlayerInRound, String secondPlayerInRound) {
         String winner;
-        //here, we try to get the first player card
+        int rankToRemove;
+        // here, we try to get the first player card
         Card cardToFirstPlayer = getCardOrGameOver(secondPlayerInRound);
         if (cardToFirstPlayer == null) {
-            //secondPlayerInRound --> winner
+            // secondPlayerInRound --> winner
             winner = secondPlayerInRound;
-            //players.remove(firstPlayerInRound);
+            // players.remove(firstPlayerInRound);
             return winner;
         }
 
@@ -91,23 +90,28 @@ public abstract class PouilleuxGameEngine {
 
         boolean checkCardOrGameOverSecond = checkCardOrGameOver(secondPlayerInRound);
         if (!checkCardOrGameOverSecond) {
-            //secondPlayerInRound --> winner
+            // secondPlayerInRound --> winner
             winner = secondPlayerInRound;
-            //players.remove(firstPlayerInRound);
+            // players.remove(firstPlayerInRound);
             return winner;
         }
 
-        //verifier la paire 
-        removePairsFromPlayer(firstPlayerInRound);
+        // verifier la paire
+        rankToRemove = removePairsFromPlayer(firstPlayerInRound);
 
         boolean checkCardOrGameOver = checkCardOrGameOver(firstPlayerInRound);
         if (!checkCardOrGameOver) {
             winner = firstPlayerInRound;
-            //players.remove(secondPlayerInRound);
+            // players.remove(secondPlayerInRound);
             return winner;
         }
 
-        //otherwise we do another round.
+        if (rankToRemove == 10) {
+            System.out.println("Paire de 10 ! Changement de sens !");
+            changePlayerTurn(players);
+        }
+
+        // otherwise we do another round.
         return "";
     }
 
@@ -123,17 +127,24 @@ public abstract class PouilleuxGameEngine {
 
     protected abstract Card getCardFromPlayer(String player) throws NoMoreCardException;
 
-    protected abstract void removePairsFromPlayer(String player); 
-    
-    protected abstract void giveOneCardToPlayer(Card card, String player); 
+    protected abstract int removePairsFromPlayer(String player);
+
+    protected abstract void giveOneCardToPlayer(Card card, String player);
 
     protected abstract List<Card> findPairs(String player);
 
     protected abstract boolean checkLoser(String player);
 
+    protected void changePlayerTurn(Deque<String> players) {
 
+        Deque<String> reversedPlayers = new LinkedList<>();
+        String lastPlayer = players.pollLast();
+        players.addFirst(lastPlayer);
 
+        while (!players.isEmpty()) {
+            reversedPlayers.addFirst(players.pollFirst());
+        }
 
-
-
+        players.addAll(reversedPlayers);
+    }
 }
