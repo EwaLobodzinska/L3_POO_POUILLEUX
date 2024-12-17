cb44acc8109bbff9ac12d6cd1bd3476b5f4dd2ee
Câm-Vi Nguyen <167216844+camviweb@users.noreply.github.com>
2024-12-17 23:36:35 +0100

    Update PouilleuxGameNetworkEngine.java

--- src/main/java/fr/pantheonsorbonne/miage/engine/net/PouilleuxGameNetworkEngine.java
+++ src/main/java/fr/pantheonsorbonne/miage/engine/net/PouilleuxGameNetworkEngine.java
@@ -22,7 +22,8 @@ public class PouilleuxGameNetworkEngine extends PouilleuxGameEngine {
     private final Set<String> players;
     private final Game pouilleux;
 
-    public PouilleuxGameNetworkEngine(Deck deck, HostFacade hostFacade, Set<String> players, fr.pantheonsorbonne.miage.model.Game pouilleux) {
+    public PouilleuxGameNetworkEngine(Deck deck, HostFacade hostFacade, Set<String> players,
+            fr.pantheonsorbonne.miage.model.Game pouilleux) {
         super(deck);
         this.hostFacade = hostFacade;
         this.players = players;
@@ -30,52 +31,47 @@ public class PouilleuxGameNetworkEngine extends PouilleuxGameEngine {
     }
 
     public static void main(String[] args) {
-        //create the host facade
         HostFacade hostFacade = Facade.getFacade();
         hostFacade.waitReady();
 
-        //set the name of the player
         hostFacade.createNewPlayer("Host");
 
-        //create a new game of war
         fr.pantheonsorbonne.miage.model.Game pouilleux = hostFacade.createNewGame("POUILLEUX");
 
-        //wait for enough players to join
         hostFacade.waitForExtraPlayerCount(PLAYER_COUNT);
 
-        PouilleuxGameEngine host = new PouilleuxGameNetworkEngine(new RandomDeck(), hostFacade, pouilleux.getPlayers(), pouilleux);
+        PouilleuxGameEngine host = new PouilleuxGameNetworkEngine(new RandomDeck(), hostFacade, pouilleux.getPlayers(),
+                pouilleux);
         host.play();
         System.exit(0);
     }
-//
+
     @Override
     protected List<String> getInitialPlayers() {
-        return new ArrayList<>(this.pouilleux.getPlayers());
+        return new ArrayList<String>(this.pouilleux.getPlayers());
     }
-//
+
     @Override
     protected void giveCardsToPlayer(String playerName, String hand) {
         hostFacade.sendGameCommandToPlayer(pouilleux, playerName, new GameCommand("cardsForYou", hand));
     }
-//
+
     @Override
     protected void declareWinner(String winner) {
         hostFacade.sendGameCommandToPlayer(pouilleux, winner, new GameCommand("gameOver", "win"));
     }
-//
+
     @Override
     protected Card getCardOrGameOver(String cardProviderPlayer) {
-
         try {
             return getCardFromPlayer(cardProviderPlayer);
         } catch (NoMoreCardException nmc) {
             return null;
         }
-
     }
-//
+
     @Override
-    protected boolean checkCardOrGameOver(String cardProviderPlayer){
+    protected boolean checkCardOrGameOver(String cardProviderPlayer) {
         hostFacade.sendGameCommandToPlayer(pouilleux, cardProviderPlayer, new GameCommand("checkACard"));
         GameCommand expectedCard = hostFacade.receiveGameCommand(pouilleux);
         if (expectedCard.name().equals("outOfCard")) {
@@ -84,16 +80,15 @@ public class PouilleuxGameNetworkEngine extends PouilleuxGameEngine {
         return true;
     }
 
-//
     @Override
     protected void giveCardsToPlayer(Collection<Card> roundStack, String player) {
         List<Card> cards = new ArrayList<>();
         cards.addAll(roundStack);
         Collections.shuffle(cards);
-        hostFacade.sendGameCommandToPlayer(pouilleux, player, new GameCommand("cardsForYou", Card.cardsToString(cards.toArray(new Card[cards.size()]))));
+        hostFacade.sendGameCommandToPlayer(pouilleux, player,
+                new GameCommand("cardsForYou", Card.cardsToString(cards.toArray(new Card[cards.size()]))));
     }
 
-//
     @Override
     protected Card getCardFromPlayer(String player) throws NoMoreCardException {
         hostFacade.sendGameCommandToPlayer(pouilleux, player, new GameCommand("getACard"));
@@ -104,21 +99,20 @@ public class PouilleuxGameNetworkEngine extends PouilleuxGameEngine {
         if (expectedCard.name().equals("outOfCard")) {
             throw new NoMoreCardException();
         }
-        //should not happen!
+        // should not happen!
         throw new RuntimeException("invalid state");
-
     }
 
-//
     @Override
     protected void giveOneCardToPlayer(Card card, String player) {
         List<Card> cards = new ArrayList<>();
         cards.add(card);
-        hostFacade.sendGameCommandToPlayer(pouilleux, player, new GameCommand("cardsForYou", Card.cardsToString(cards.toArray(new Card[cards.size()]))));
+        hostFacade.sendGameCommandToPlayer(pouilleux, player,
+                new GameCommand("cardsForYou", Card.cardsToString(cards.toArray(new Card[cards.size()]))));
     }
 
     @Override
-    protected int removePairsFromPlayer(String player, List<Integer> tourColor){
+    protected int removePairsFromPlayer(String player, List<Integer> tourColor) {
         List<Card> pairs = findPairs(player);
         int rankToRemove = 0;
 
@@ -128,7 +122,7 @@ public class PouilleuxGameNetworkEngine extends PouilleuxGameEngine {
             if (expectedHand.name().equals("cards")) {
                 Card[] cards = Card.stringToCards(expectedHand.body());
                 Queue<Card> originalQueue = new LinkedList<>();
-                for(Card card : cards){
+                for (Card card : cards) {
                     originalQueue.offer(card);
                 }
                 Queue<Card> updatedQueue = new LinkedList<>();
@@ -141,43 +135,46 @@ public class PouilleuxGameNetworkEngine extends PouilleuxGameEngine {
                 for (Card card : originalQueue) {
                     if (card.getValue().getRank() != rankToRemove ||
                             (card.getColor().getCode() != codeToRemove[0]
-                                    && card.getColor().getCode() != codeToRemove[1])){
+                                    && card.getColor().getCode() != codeToRemove[1])) {
                         updatedQueue.add(card);
-                    } else if(tourColor != null && 
-                            (card.getColor().getCode() == tourColor.get(0) 
-                                    || card.getColor().getCode() == tourColor.get(1))){
+                    } else if (tourColor != null &&
+                            (card.getColor().getCode() == tourColor.get(0)
+                                    || card.getColor().getCode() == tourColor.get(1))) {
                         updatedQueue.add(card);
-                    //} else {
-                    } else if(rankToRemove != 10 && rankToRemove != 11 && rankToRemove != 12 &&rankToRemove != 13 &&rankToRemove != 14){
+                        // } else {
+                    } else if (rankToRemove != 10 && rankToRemove != 11 && rankToRemove != 12 && rankToRemove != 13
+                            && rankToRemove != 14) {
                         System.out.println(card.toString());
                     }
                 }
-
-                hostFacade.sendGameCommandToPlayer(pouilleux, player, new GameCommand("clearHand"));
-                giveCardsToPlayer(updatedQueue, player);
+                hostFacade.sendGameCommandToPlayer(pouilleux, player, new GameCommand("cardsWithoutPair",
+                        Card.cardsToString(updatedQueue.toArray(new Card[updatedQueue.size()]))));
+                // hostFacade.sendGameCommandToPlayer(pouilleux, player, new
+                // GameCommand("cardsWithoutPair"));
+                // giveCardsToPlayer(updatedQueue, player);
             }
         }
         return rankToRemove;
     }
 
     @Override
-    protected List<Card> findPairs(String player){
+    protected List<Card> findPairs(String player) {
         hostFacade.sendGameCommandToPlayer(pouilleux, player, new GameCommand("getAHand"));
         GameCommand expectedHand = hostFacade.receiveGameCommand(pouilleux);
         if (expectedHand.name().equals("cards")) {
             Card[] cardsReceived = Card.stringToCards(expectedHand.body());
             Queue<Card> cards = new LinkedList<>();
-            for(Card card : cardsReceived){
+            for (Card card : cardsReceived) {
                 cards.offer(card);
             }
             Map<Integer, Integer> blackCardCount = new HashMap<>();
             Map<Integer, Integer> redCardCount = new HashMap<>();
             List<Card> pair = new ArrayList<>();
-    
+
             for (Card card : cards) {
                 int cardRank = card.getValue().getRank();
                 int cardCode = card.getColor().getCode();
-    
+
                 if (cardCode == 127137 || cardCode == 127137 + 16 * 3) {
                     if (blackCardCount.containsKey(cardRank)) {
                         blackCardCount.put(cardRank, blackCardCount.get(cardRank) + 1);
@@ -185,14 +182,14 @@ public class PouilleuxGameNetworkEngine extends PouilleuxGameEngine {
                         blackCardCount.put(cardRank, 1);
                     }
                     // blackCardCount.put(cardRank, blackCardCount.getOrDefault(cardRank, 0) + 1);
-    
+
                     if (blackCardCount.get(cardRank) == 2) {
                         for (Card cardPair : cards) {
                             if (cardPair.getValue().getRank() == cardRank && (cardPair.getColor().getCode() == 127137
                                     || cardPair.getColor().getCode() == 127137 + 16 * 3)) {
                                 pair.add(cardPair);
                                 if (pair.size() == 2) {
-                                    return pair; 
+                                    return pair;
                                 }
                             }
                         }
@@ -204,11 +201,12 @@ public class PouilleuxGameNetworkEngine extends PouilleuxGameEngine {
                         redCardCount.put(cardRank, 1);
                     }
                     // redCardCount.put(cardRank, redCardCount.getOrDefault(cardRank, 0) + 1);
-    
+
                     if (redCardCount.get(cardRank) == 2) {
                         for (Card cardPair : cards) {
-                            if (cardPair.getValue().getRank() == cardRank && (cardPair.getColor().getCode() == 127137 + 16
-                                    || cardPair.getColor().getCode() == 127137 + 16 * 2)) {
+                            if (cardPair.getValue().getRank() == cardRank
+                                    && (cardPair.getColor().getCode() == 127137 + 16
+                                            || cardPair.getColor().getCode() == 127137 + 16 * 2)) {
                                 pair.add(cardPair);
                                 if (pair.size() == 2) {
                                     return pair;
@@ -218,12 +216,13 @@ public class PouilleuxGameNetworkEngine extends PouilleuxGameEngine {
                     }
                 }
             }
+            return null;
         }
         return null;
     }
 
     @Override
-    protected boolean checkLoser(String player){
+    protected boolean checkLoser(String player) {
         hostFacade.sendGameCommandToPlayer(pouilleux, player, new GameCommand("getAHand"));
         GameCommand expectedHand = hostFacade.receiveGameCommand(pouilleux);
         if (expectedHand.name().equals("cards")) {
